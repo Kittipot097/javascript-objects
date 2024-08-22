3c939df8cc2d39ad6e4c460a8352404aec47f0bb
jerpuen <kittipot.kulpinit@gmail.com>
2024-08-22 11:06:08 +0700

    sent: assignment ex1 and ex2 and fixed ex3

--- ex-2.js
+++ ex-2.js
@@ -8,3 +8,19 @@ const product = {
 };
 let socialMediaShare = "facebookShare";
 // Start code here
+product.ratings = 4.5
+product.photo = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+console.log(product.ratings);
+console.log(product.photo);
+let va1 = "code";
+product[va1] = "IT010101";
+console.log(product.code);
+product.price = 6000;
+console.log(product.price);
+delete product.ratings;
+console.log(product.ratings);
+let v2 = "socialMediaShare";
+product[v2] = 45.5;
+console.log(product.socialMediaShare);
+console.log(product);
+
